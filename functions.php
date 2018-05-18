f49485ca17dfd6c336b9d77df1a9a2208354f65e
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-05-18 16:51:32 +0600

    Updating to latest stable version.

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Bangla Font Fixer
-Plugin URI: http://bff.bhinno.net/
+Plugin URI: https://tauhid.xyz/my-creations/bangla-font-fixer/
 Description: বাংলা ফন্ট ভেঙে যাওয়ার সমস্যা থেকে পরিত্রাণ পান।
-Author: Tasnimul Hasan Tauhid
-Version: 2.0
-Author URI: http://www.facebook.com/tht52
+Author: Tasnimul H. Tauhid
+Version: 2.1
+Author URI: https://tauhid.xyz/
 */
 
 //For custom font in wp_head and front page body
@@ -16,10 +16,10 @@ function bffbtht_wp_head_font() {
   echo '<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosansbengali.css" />
   <style>
     html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video, textarea, input, select {
-      font-family: "Noto Sans Bengali", SolaimanLipi, Vrinda;
+      font-family: "Noto Sans Bengali";
     } 
 #wpadminbar #wp-admin-bar-my-sites a.ab-item, #wpadminbar #wp-admin-bar-site-name a.ab-item #wpadminbar .quicklinks .ab-empty-item, #wpadminbar .quicklinks a, #wpadminbar .shortlink-input{
-      font-family: "Noto Sans Bengali", SolaimanLipi, Vrinda;
+      font-family: "Noto Sans Bengali";
     } 
   </style>';
 }
@@ -32,10 +32,10 @@ function bffbtht_admin_font() {
   echo '<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosansbengali.css" />
   <style>
     body, td, textarea, input, select {
-      font-family: "Noto Sans Bengali", SolaimanLipi, Vrinda;
+      font-family: "Noto Sans Bengali";
     } 
 #wpadminbar #wp-admin-bar-my-sites a.ab-item, #wpadminbar #wp-admin-bar-site-name a.ab-item #wpadminbar .quicklinks .ab-empty-item, #wpadminbar .quicklinks a, #wpadminbar .shortlink-input{
-      font-family: "Noto Sans Bengali", SolaimanLipi, Vrinda;
+      font-family: "Noto Sans Bengali";
     } 
   </style>';
 }
@@ -47,6 +47,6 @@ add_action('login_head', 'bffbtht_login_font');
 function bffbtht_login_font() {
   echo '<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosansbengali.css" />
   <style>
-p {font-family: "Noto Sans Bengali", SolaimanLipi, Vrinda;} 
+p {font-family: "Noto Sans Bengali";} 
   </style>';
 }
--- readme.txt
+++ readme.txt
@@ -1,21 +1,28 @@
 === Bangla Font Fixer ===
 Contributors: tht52
-Tags: bangla, font, fix, fixer, break down
-Requires at least: 3.0
-Tested up to: 4.2.2
-Stable tag: 4.3
+Tags: bangla, font, fix, fixer, break down, বাংলা, ফন্ট, ফিক্সার
+Requires at least: 0.7
+Tested up to: 5.0
+Stable tag: 2.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-বাংলা ফন্ট ভেঙে যাওয়ার সমস্যা থেকে পরিত্রাণ পান।
+বাংলা ফন্ট এর সকল সমস্যা থেকে মুক্তি পান।
+Eliminate all bangla font problem.
 
 == Description ==
 
-
-আমরা সকলেই জানি যে বাঙলা ফন্ট যেকোনো ব্রাউজারে ভেঙে যায়। এই সমস্যা থেকে পরিত্রাণ পাওয়ার জন্য আপনাদের জন্য তৈরি করেছি বাংলা ফন্ট ফিক্সার। 
+আমরা সকলেই জানি যে বাংলা ফন্ট যেকোনো ব্রাউজারে ভেঙে যায়। এই সমস্যা থেকে পরিত্রাণ পাওয়ার জন্য আপনাদের জন্য তৈরি করেছি বাংলা ফন্ট ফিক্সার। 
 
 <b>প্লাগিনটি কি ভাবে কাজ করে?</b>
-এই প্লাগিনটি আপনার সাইটের সকল বাংলা লেখাকে সোলাইমানলিপি ফন্ট করে দেবে। উল্লেখ্য যে সোলাইমানলিপি ফন্ট অধিকাংশ উন্নত বাংলা ওয়েবসাইটে ব্যাবহার করা হয়।
+এই প্লগিনটি আপনার সাইটের সকল ফন্টকে বাংলা ফন্টে রূপান্তর করবে। সক্ষমতার জন্য ব্যবহার করা হয়েছে গুগোল ফন্ট। যার ফলে আপনি নিশ্চিন্তে থাকতে পারেন যে আপনার ওয়োবসাইটের ফন্ট দেশ কাল পাত্র কম্পিউটার ভেদে ফন্ট ঠিক রাখবে।
+
+We all Know that Bengali fonts break in many browser. Specially in Google Chrome Browser.
+
+<b>How Bangla Font Fixer Works?</b>
+Bangla Font Fixer plugin will convert all the fonts of your website to Bengali Font. It is powered by Google Fonts. So you can rely on this plugin. 
+
+<b>প্লাগিনটির যাবতীয় হালনাগাদ তথ্য পাবেন পাবেন <a href="https://tauhid.xyz/my-creations/bangla-font-fixer/" target="blank">এখানে</a><b/>
 
 == Installation ==
 
@@ -24,11 +31,18 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 == Frequently Asked Questions ==
 
-= আমি প্লাগিনটি ইন্সটল করেছি তবে ফন্ট এখনো ভেঙে যাচ্ছে =
+= আমার কি নতুন করে কোনোও ফল্ট ইন্সটল করতে হব? =
+না। Bangla Font Fixer গুগোল ফন্ট ব্যবহার করে তাই কোনোও ফন্ট ইন্সটল করার ঝামেলা নেই। 
 
-সোলাইমানলিপি (SolaimanLipi) ফন্টটি ইন্সটল করে নিন। আশা করি সমস্যা সমাধান হয়ে যাবে।
-
+= প্লাগইনটিতে কোন ফন্ট ব্যবহার করা হয়েছে? =
+প্লাগইনটিতে ব্যবহার করা হয়েছে "Noto Sans Bengali"
 == Changelog ==
 
 = 1.0 =
 * প্রথম প্রকাশ করা হল।
+= 1.0.1 =
+বৃন্দা ফন্ট ফল-ব্যাক হিসাবে যুক্ত করা হল।
+= 1.1 =
+আরও অনেক ট্যাগের জন্য বাংলা ফন্ট ফিক্সারকে উন্নয়ন করা হলো।
+= 2.0 =
+নোটো স্যান্স বেন্গলি যুক্ত করা হলো ও সোলাইমানলিপিকে ফল-ব্যাক করা হলো।
